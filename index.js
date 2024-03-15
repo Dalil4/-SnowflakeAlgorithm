8c44d34a4816f6ff9f15c02bb88c99603622bd41
Dalil4 <122190551+Dalil4@users.noreply.github.com>
2024-03-16 00:19:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
+
 class Snowflake {
     constructor(workerId, datacenterId, sequence = 0) {
         this.workerId = workerId;
         this.datacenterId = datacenterId;
         this.sequence = sequence;
-
-        // Snowflake Epoch (January 1, 2020)
         this.epoch = 1577836800000;
         this.timestampBits = 41;
         this.workerIdBits = 5;
         this.datacenterIdBits = 5;
         this.sequenceBits = 12;
-
         this.maxWorkerId = -1 ^ (-1 << this.workerIdBits);
         this.maxDatacenterId = -1 ^ (-1 << this.datacenterIdBits);
-
         this.workerIdShift = this.sequenceBits;
         this.datacenterIdShift = this.sequenceBits + this.workerIdBits;
         this.timestampLeftShift = this.sequenceBits + this.workerIdBits + this.datacenterIdBits;
         this.sequenceMask = -1 ^ (-1 << this.sequenceBits);
-
         this.lastTimestamp = -1;
     }
 
     generate() {
         let timestamp = Date.now();
-
         if (timestamp < this.lastTimestamp) {
             throw new Error('Invalid system clock');
         }
-
         if (timestamp === this.lastTimestamp) {
             this.sequence = (this.sequence + 1) & this.sequenceMask;
             if (this.sequence === 0) {
@@ -37,13 +31,8 @@ class Snowflake {
         } else {
             this.sequence = 0;
         }
-
         this.lastTimestamp = timestamp;
-
-        return ((timestamp - this.epoch) << this.timestampLeftShift) |
-            (this.datacenterId << this.datacenterIdShift) |
-            (this.workerId << this.workerIdShift) |
-            this.sequence;
+        return ((timestamp - this.epoch) << this.timestampLeftShift) | (this.datacenterId << this.datacenterIdShift) | (this.workerId << this.workerIdShift) | this.sequence;
     }
 
     waitNextMillis() {
@@ -53,8 +42,23 @@ class Snowflake {
         }
         return timestamp;
     }
+
+    parseSnowflake(snowflakeId) {
+        const mask = (1 << this.timestampBits) - 1;
+        const timestamp = new Date((snowflakeId >> this.timestampLeftShift) + this.epoch);
+        const datacenterId = (snowflakeId >> this.datacenterIdShift) & this.maxDatacenterId;
+        const workerId = (snowflakeId >> this.workerIdShift) & this.maxWorkerId;
+        const sequence = snowflakeId & this.sequenceMask;
+        return { timestamp, datacenterId, workerId, sequence };
+    }
+
+    isValidSnowflake(snowflakeId) {
+        const timestamp = (snowflakeId >> this.timestampLeftShift) + this.epoch;
+        return timestamp <= Date.now();
+    }
 }
 
-// Example usage:
 const snowflake = new Snowflake(1, 1);
-console.log(snowflake.generate());
+const id = snowflake.generate();
+const parsedId = snowflake.parseSnowflake(id);
+const isValid = snowflake.isValidSnowflake(id);
